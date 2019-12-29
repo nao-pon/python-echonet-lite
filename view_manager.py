9d982f5f15a1fdf8701a4c6939ecf5208924358d
Katsumi Noguchi <noguchi@KatsuminoMacBook-Pro.local>
2019-12-30 08:39:17 +0900

    bug fix

--- view_manager.py
+++ view_manager.py
@@ -9,14 +9,16 @@ from luma.core import cmdline, error
 from luma.core.render import canvas
 from PIL import ImageFont
 import netifaces
-from property_manager import PropertyManager 
+from property_manager import PropertyManager
+
 
 class ViewManager(metaclass=ABCMeta):
     "Viewマネージャ"
 
     # 初期化
     def __init__(self):
-        self._device = self.get_device(['--config','/home/pi/wisun-gateway/display.conf'])
+        self._device = self.get_device(
+            ['--config', '/home/pi/wisun-gateway/display.conf'])
         self._width = min(self._device.width, 240)
         self._height = min(self._device.height, 240)
         self._font = self.make_font("code2000.ttf", 10)
@@ -24,7 +26,8 @@ class ViewManager(metaclass=ABCMeta):
         self._pm = None
         with canvas(self._device) as draw:
             text_w, text_h = draw.textsize('t', self._font)
-            self._payloadArea = (0, text_h, self._width - 1, self._height - 1 - text_h)
+            self._payloadArea = (0, text_h, self._width -
+                                 1, self._height - 1 - text_h)
 
     # Propertyマネージャ設定
     def setPropertyManager(self, pm):
@@ -42,16 +45,20 @@ class ViewManager(metaclass=ABCMeta):
             # 外枠
             # draw.rectangle((0, 0, self._width - 1, self._height - 1), outline="blue")
             # draw.rectangle((self._payloadArea[0],self._payloadArea[1]-1,self._payloadArea[2],self._payloadArea[3]+1), outline="blue")
-            draw.line((self._payloadArea[0],self._payloadArea[1]-1,self._payloadArea[2],self._payloadArea[1]-1), fill="blue")
-            draw.line((self._payloadArea[0],self._payloadArea[3]+1,self._payloadArea[2],self._payloadArea[3]+1), fill="blue")
+            draw.line((self._payloadArea[0], self._payloadArea[1]-1,
+                       self._payloadArea[2], self._payloadArea[1]-1), fill="blue")
+            draw.line((self._payloadArea[0], self._payloadArea[3]+1,
+                       self._payloadArea[2], self._payloadArea[3]+1), fill="blue")
 
             # 日付
             now = datetime.datetime.now()
             self._date = now
-            dt = u"'{0:02}/{1:02}/{2:02} {3:02}:{4:02}:{5:02}".format(now.year % 100, now.month, now.day, now.hour, now.minute, now.second)
+            dt = u"'{0:02}/{1:02}/{2:02} {3:02}:{4:02}:{5:02}".format(
+                now.year % 100, now.month, now.day, now.hour, now.minute, now.second)
             # date_w, date_h = draw.textsize("'88/88/88 88:88:88", self._font)
             date_w, date_h = draw.textsize(dt, self._font)
-            draw.text(((self._width - date_w)/2, self._height - date_h), dt, fill="yellow", font=self._font)
+            draw.text(((self._width - date_w)/2, self._height -
+                       date_h), dt, fill="yellow", font=self._font)
             # IP Address更新
             self.refleshIpAddr(draw)
             # ペイロード更新
@@ -62,19 +69,22 @@ class ViewManager(metaclass=ABCMeta):
         addr = None
         try:
             addr = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
-        except KeyError:
+        except Exception:
             try:
-                addr = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
-            except KeyError:
+                addr = netifaces.ifaddresses(
+                    'wlan0')[netifaces.AF_INET][0]['addr']
+            except Exception:
                 pass
         if addr is not None:
             addr_w, addr_h = draw.textsize(addr, self._font)
-            draw.text(((self._width - addr_w)/2, 0), addr, fill="yellow", font=self._font)
+            draw.text(((self._width - addr_w)/2, 0), addr,
+                      fill="yellow", font=self._font)
 
     # ペイロード表示のクリア
     def clearPayload(self):
         with canvas(self._device) as draw:
-            draw.rectangle((self._payloadArea[0],self._payloadArea[1],self._payloadArea[2],self._payloadArea[3]), fill="black", outline="black")
+            draw.rectangle((self._payloadArea[0], self._payloadArea[1],
+                            self._payloadArea[2], self._payloadArea[3]), fill="black", outline="black")
 
     @abstractmethod
     # ペイロードの表示
@@ -104,6 +114,7 @@ class ViewManager(metaclass=ABCMeta):
 
     def dispose(self):
         pass
+
 
 class ViewManagerAnalog(ViewManager):
     def __init__(self):
@@ -138,11 +149,14 @@ class ViewManagerAnalog(ViewManager):
             # secs = posn(sec_angle, cy - margin - 2)
             secs = self.posn(sec_angle, radius - 2)
             # reflet view
-            draw.ellipse((cx - radius , cy - radius, cx + radius, cy + radius), outline="white")
+            draw.ellipse((cx - radius, cy - radius, cx +
+                          radius, cy + radius), outline="white")
             draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
             draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
             draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
-            draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill="white", outline="white")
+            draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2),
+                         fill="white", outline="white")
+
 
 if __name__ == "__main__":
     vm = ViewManagerAnalog()
