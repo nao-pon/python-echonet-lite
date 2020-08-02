4b34f272973f10f536afa74123ac22155d12e9b0
katsumin <katsumi.noguchi@gmail.com>
2020-08-02 16:19:10 +0900

    refactoring

--- influx_manager.py
+++ influx_manager.py
@@ -8,6 +8,7 @@ from influxdb import InfluxDBClient
 from logging import getLogger, StreamHandler, INFO, Formatter
 logger = getLogger(__name__)
 
+
 class InfluxManager:
     def __init__(self):
         # 積算電力係数
@@ -17,7 +18,8 @@ class InfluxManager:
         iniFile.read('/home/pi/wisun-gateway/config.ini')
         try:
             url = iniFile.get('influxdb', 'url')
-            self._client = InfluxDBClient(url, 8086, 'root', 'roor', 'smartmeter')
+            self._client = InfluxDBClient(
+                url, 8086, 'root', 'root', 'smartmeter')
         except (NoSectionError, NoOptionError):
             self._client = None
 
@@ -31,18 +33,26 @@ class InfluxManager:
                 text += 'power value={0} {1}\n'.format(values[0], timestamp)
             elif p.EPC == 0xea:
                 values = struct.unpack('>HBBBBBL', p.EDT)
-                d = time.strptime("{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(values[0],values[1],values[2],values[3],values[4],values[5]), "%Y/%m/%d %H:%M:%S")
+                d = time.strptime("{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(
+                    values[0], values[1], values[2], values[3], values[4], values[5]), "%Y/%m/%d %H:%M:%S")
                 timestamp = int(time.mktime(d)) * 1000000000
-                text += '+power value={0} {1}\n'.format(values[6] * 0.1, timestamp)
+                text += '+power value={0} {1}\n'.format(
+                    values[6] * 0.1, timestamp)
             elif p.EPC == 0xeb:
                 values = struct.unpack('>HBBBBBL', p.EDT)
-                d = time.strptime("{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(values[0],values[1],values[2],values[3],values[4],values[5]), "%Y/%m/%d %H:%M:%S")
+                d = time.strptime("{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(
+                    values[0], values[1], values[2], values[3], values[4], values[5]), "%Y/%m/%d %H:%M:%S")
                 timestamp = int(time.mktime(d)) * 1000000000
-                text += '-power value={0} {1}\n'.format(values[6] * 0.1, timestamp)
+                text += '-power value={0} {1}\n'.format(
+                    values[6] * 0.1, timestamp)
         d = datetime.datetime.today()
-        f = open('/home/pi/wisun-gateway/power{0:04d}{1:02d}{2:02d}.txt'.format(d.year,d.month,d.day), 'a')
+        f = open(
+            '/home/pi/wisun-gateway/power{0:04d}{1:02d}{2:02d}.txt'.format(d.year, d.month, d.day), 'a')
         f.write(text)
         f.close()
         if self._client is not None:
-            self._client.write_points(text, protocol='line')
+            try:
+                self._client.write_points(text, protocol='line')
+            except Exception:
+                pass
         logger.info(text)
